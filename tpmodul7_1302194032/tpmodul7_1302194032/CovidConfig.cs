e19ed8d13f5dadaf809c893368ba6b4e0a404112
ANWAR YUSUF <anwaryusuf@365.telkomuniversity.ac.id>
2022-04-08 17:00:36 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul7_1302194032/tpmodul7_1302194032/CovidConfig.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_1302194032
+{
+    internal class CovidConfig
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+            .AddViewLocalization(LanguageViewLocationExpanderFormat.Suf
+            fix);
+            services.AddPortableObjectLocalization();
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new List<CultureInfo>
+{
+new CultureInfo("en-US"),
+new CultureInfo("en"),
+new CultureInfo("fr-FR"),
+new CultureInfo("fr")
+};
+
+                options.DefaultRequestCulture = new RequestCulture("en-
+                US");
+
+
+
+                options.SupportedCultures = supportedCultures;
+                options.SupportedUICultures = supportedCultures;
+            });
+        }
+    }
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment
+env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+        }
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseRequestLocalization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(name: "default", pattern:
+            "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
